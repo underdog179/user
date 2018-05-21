bd4dc1275379734cec5352bca00405a0282e20bf
gorgear <mac@macdeMacBook-Air.local>
2018-05-21 15:48:58 +0800

    update: 完成了简答的github登录方式

--- controller/index.js
+++ controller/index.js
@@ -4,9 +4,11 @@ const config = require('../config');
 const request = require('superagent');
 const _ = require('lodash');
 const co = require('co');
+const log = require('../common/log');
 
 /**
  * @api /login
+ * @description 多种方式的登录，目前只实现的github
  */
 exports.login = function (req, callback) {
   callback(null, {
@@ -19,6 +21,14 @@ exports.login = function (req, callback) {
 };
 
 /**
+ * @api /login/github
+ * @description 使用github oauth进行登录
+ */
+exports.githubOauth = function (req) {
+  req.redirect(`https://github.com/login/oauth/authorize?client_id=${config.id}&scope=user,public_repo`);
+};
+
+/**
  * @api /oauth2/github/callback
  * @description 用于处理github oauth返回的结果
  */
@@ -27,16 +37,21 @@ exports.githubCb = function (req, callback) {
   const code = query.code;
   const id = _.get(config, 'oauth.github.id');
   const secret = _.get(config, 'oauth.github.secret');
+  log.info('begin to request');
   co(function* () {
     const result = yield request.get(`https://github.com/login/oauth/access_token?client_id=${id}&client_secret=${secret}&code=${code}`);
     const astoken = _.get(result, 'body.access_token');
+    log.info('request access_token success', astoken);
     if (astoken) {
-      const user = yield request.get(`https://api.github.com/user?access_token=${astoken}`).body;
+      const user = (yield request.get(`https://api.github.com/user?access_token=${astoken}`)).body;
       req.session.user = user;
-      callback(null);
+      log.info('request user info success', user);
+      req.redirect('/');
+    } else {
+      callback('NO_ACCESS_TOKEN');
     }
   }).catch(e => {
-    callback(e);
+    callback(e.stack || e.message || e);
   });
 };
 
@@ -51,4 +66,3 @@ exports.getUser = function (req, callback) {
     callback('UNAUTHED');
   }
 };
-
